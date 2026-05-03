2247ceccd30c8fc41d5069d74aedc360bd025554
Ksenon-chik <your_email@example.com>
2026-05-03 19:49:37 +0500

    postgre

--- db/session.py
+++ db/session.py
@@ -1,21 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
 # подключение к базе данных
-SQLALCHEMY_DATABASE_URL = "sqlite:///./investment_tracker.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql://user:password@db:5432/investment_db"
+)
 
 # движок SQLAlchemy
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args = {'check_same_thread': False}
-)
+engine = create_engine(DATABASE_URL)
 
 
 # сессии
 SessionLocal = sessionmaker(
-    autocommit = False,
-    autoflush = False,
-    bind = engine
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
 
 def get_db():
